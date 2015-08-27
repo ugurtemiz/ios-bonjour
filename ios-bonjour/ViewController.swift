b3162dddc4b56acd98dded118f113de21223e62b
Ugur Temiz <ugurtemiz@Ugurs-MacBook-Pro.local>
2015-08-27 23:51:21 +0300

    Initial commit. CocoaAsyncSocket is added as library.

---
+++ ios-bonjour/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  ios-bonjour
+//
+//  Created by Ugur Temiz on 27/08/15.
+//  Copyright (c) 2015 Ugur Temiz. All rights reserved.
+//
+
+import CocoaAsyncSocket
+import UIKit
+
+enum TAG: Int {
+    case header = 1
+    case body   = 2
+}
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
